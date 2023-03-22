2e6e8ba7c037e4dbdf6f567c17918a85958e4331
Polina Sergacheva <malaya160901@mail.ru>
2023-03-22 12:07:23 +0700

    Обновление функции для нахождения суммы директории

--- Hash_directory.py
+++ Hash_directory.py
@@ -1,13 +1,37 @@
 import hashlib
+import os
 from os import listdir
 
 dir_hash = ''
-for f in listdir('/home/spi_729-1/Документы/Test/'):
-    with open('/home/spi_729-1/Документы/Test/'+f, "rb") as f:
-        file_hash = hashlib.sha256()  # Создаем объект хеша
-        while chunk := f.read(8192):
-            file_hash.update(chunk)
 
-            dir_hash += file_hash.hexdigest()
+# print(hashlib.sha256(dir_hash.encode('latin_1')).hexdigest())
+# print("Папки и файлы данного каталога: ", listdir('/home/spi_729-1/Документы/Test/'))  #  Возвращает список файлов и каталогов
 
-print(hashlib.sha256(dir_hash.encode('latin_1')).hexdigest())
+# Функция для получения хеш-суммы файла
+def hash_file(file_name):
+    h = hashlib.sha256()  # Создание объекта хеша
+
+    #  Открытие файла для чтения в двоичном режиме
+    with open(file_name, 'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)  # Циклом читаем до конца файла только 1024 байта за раз
+            h.update(chunk)
+
+    return h.hexdigest()
+
+summ = 0
+
+#  Распечатать все файлы и папки рекурсивно
+for dirpath, dirnames, filenames in os.walk("/home/spi_729-1/Документы/Test/"):
+    # перебрать каталоги
+    for dirname in dirnames:
+        print("Каталог: ", os.path.join(dirpath, dirname))
+    # перебрать файлы
+    for filename in filenames:
+        print("Файл: ", os.path.join(dirpath, filename))
+        message = hash_file(os.path.join(dirpath, filename))
+        print("Хеш-сумма: ", message)
+        #  summ += message
+
+# print(summ)
