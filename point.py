9f46487017ff4f573cb3e78ed0ff8ccf4938e2a2
Polina Sergacheva <malaya160901@mail.ru>
2023-05-03 00:52:25 +0700

    Add 3D fields, new class

--- point.py
+++ point.py
@@ -12,3 +12,22 @@ class Point:
     def __str__(self):
         str_repr = 'Point({0:.5f}, {1:.5f})'.format(self.x, self.y)
         return str_repr
+
+    def hello(self):
+        return 'Hello!!! My coords are: {0:.5f}, {1:.5f}:>)'.format(self.x, self.y)
+
+# Create new Class (use inheritance)
+class Point3D(Point):
+    def __init__(self, x, y, z):
+        Point.__init__(self, x, y)  # init fields (use class Point)
+        self.z = z
+
+    def distance3d_to(self, point):
+        return ((self.x - point.x)**2 + (self.y - point.y)**2 + (self.z - point.z)**2)**0.5
+
+    def __str__(self):
+        str_repr = 'Point({0:.5f}, {1:.5f}, {2:.5f})'.format(self.x, self.y, self.z)
+        return str_repr
+
+    def hello(self):
+        return 'Hello!!! My coords are: {0:.5f}, {1:.5f}, {2:.5f} :>)'.format(self.x, self.y, self.z)
--- test_object.py
+++ test_object.py
@@ -1,16 +1,28 @@
 import point as pt
 
-point1 = pt.Point(1, 33)
-point2 = pt.Point(4, -9)
 
+# print(point1.x, point1.y)
+# print(point2.x, point2.y)
+#
+# print(point1.distance_to(point2))
+# print(point2.distance_to(point1))
+# print(point1.distance_to(point1))
+# print(point2.distance_to(point2))
+#
+# print(point1)
+# print(point2)
 
-print(point1.x, point1.y)
-print(point2.x, point2.y)
+point3D_1 = pt.Point3D(2, 4, 9)
+point3D_2 = pt.Point3D(5, 14, -29)
 
-print(point1.distance_to(point2))
-print(point2.distance_to(point1))
-print(point1.distance_to(point1))
-print(point2.distance_to(point2))
+print(point3D_1.z)
+print(point3D_1.distance_to(point3D_2))
+print(point3D_1.distance3d_to(point3D_2))
 
-print(point1)
-print(point2)
+print(point3D_1)
+print(point3D_1.hello())
+
+point1 = pt.Point(2, 4)
+point2 = pt.Point(5, 14)
+
+print(point1.hello())
