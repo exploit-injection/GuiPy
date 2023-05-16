7e382bf96f4923ef668343b97c14c506fb72b89f
Polina Sergacheva <malaya160901@mail.ru>
2023-05-17 01:10:17 +0700

    Пытаюсь сделать вывод всех файлов в каталоге

--- main.py
+++ main.py
@@ -14,25 +14,43 @@ class ExampleApp(QtWidgets.QMainWindow, control.Ui_MainWindow):
         self.setupUi(self)  # Это нужно для инициализации дизайна
         self.btnChoosedir.clicked.connect(self.choose_dir)  # инициализация для метода по нажатию кнопки "Добавить каталог"
         self.btnChoosefile.clicked.connect(self.choose_file)  # инициализация для метода по нажатию кнопки "Добавить файлы"
-        self.btnDel.clicked.connect(self.delete_item)
+        self.btnDel.clicked.connect(self.delete_item)  # инициализация для метода по нажатию кнопки "Удалить файлы"
 
+    # Функция для выбора каталога
     def choose_dir(self):
         directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
         # открыть диалог выбора директории и установить значение переменной
         # равной пути к выбранной директории
 
-        if directory: # не продолжать выполнение, если пользователь не выбрал директорию
-            for file_name in os.listdir(directory):  # для каждого файла в директории
-                self.listWidgetChoose.addItem(file_name)  # добавить файл в textFileChoose
+        # перебор элементов в окне выбора файлов
+        itemtextlist = [str(self.listWidgetChoose.item(i).text()) for i in range(self.listWidgetChoose.count())]
 
+
+
+        # Вывести в окно все файлы и папки рекурсивно
+        for dir_path, dir_names, file_names in os.walk(directory):
+
+            # перебрать каталоги
+            for dir_name in dir_names:
+                dir = os.path.join(dir_path, dir_name)
+                if dir in itemtextlist:
+                    QMessageBox.information(self, 'Внимание', f'Ранее каталог {dir} и все его файлы были добавлены', QMessageBox.Ok)
+                else:
+                    self.listWidgetChoose.addItem(dir)
+
+            # перебрать файлы
+            for file_name in file_names:
+                file = os.path.join(dir_path, os.path.join(dir_path, file_name))
+                if file in itemtextlist:
+                    return  # так как с QMessageBox будет выводиться очень много окон
+                else:
+                    self.listWidgetChoose.addItem(file)
+
+    # Функция выбора файла
     def choose_file(self):
-        files, x = QtWidgets.QFileDialog.getOpenFileNames(self, 'Выберите файлы', '/home', 'txt file (*.txt);;jpg file (*.jpg)')
-        #print(count)
+        files, x = QtWidgets.QFileDialog.getOpenFileNames(self, 'Выберите файлы', '/home/spi_729-1/Документы')
+
         for file in files:
-            # path = file  # определяем полный путь к файлу
-            # dirpath, filename = os.path.split(path)  # отделяем путь к файлу и название файла
-
-
             # Проверка на существование файла в окне выбора
             itemtextlist = [str(self.listWidgetChoose.item(i).text()) for i in range(self.listWidgetChoose.count())]  # перебор элементов в окне выбора файлов
             if file in itemtextlist:
@@ -40,12 +58,16 @@ class ExampleApp(QtWidgets.QMainWindow, control.Ui_MainWindow):
             else:
                 self.listWidgetChoose.addItem(file)  # выводим название файла и полный его путь
 
+    # Функция удаления файлов из списка
     def delete_item(self):
         list_items = self.listWidgetChoose.selectedItems()
         if not list_items:
             QMessageBox.information(self, 'Внимание', f'Вы не выбрали файл для удаления! Повторите попытку', QMessageBox.Ok)
         for item in list_items:
             self.listWidgetChoose.takeItem(self.listWidgetChoose.row(item))
+
+
+
 
 
 
